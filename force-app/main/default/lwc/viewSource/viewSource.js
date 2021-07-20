ca86e41dd0c94e8b8c6c2157cc2c45a855a95e6e
horijuan <84040063+horijuan@users.noreply.github.com>
2021-07-19 22:31:04 -0300

    Update viewSource.js

--- force-app/main/default/lwc/viewSource/viewSource.js
+++ force-app/main/default/lwc/viewSource/viewSource.js
@@ -1,12 +1,9 @@
-import { LightningElement, api } from 'lwc';
-
-export default class ViewSource extends LightningElement {
-    baseURL =
-        'https://github.com/trailheadapps/lwc-recipes/tree/main/force-app/main/default/';
-
-    @api source;
-
-    get sourceURL() {
-        return this.baseURL + this.source;
-    }
+import { LightningElement } from 'lwc';
+export default class App extends LightningElement {
+   name = 'Electra X4';
+   description = 'A sweet bike built for comfort.';
+   category = 'Mountain';
+   material = 'Steel';
+   price = '$2,700';
+   pictureUrl = 'https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/electrax4.jpg';
 }
